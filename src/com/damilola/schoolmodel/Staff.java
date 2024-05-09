07ed76b7864431e03ca4d432121e85e8666109d2
Michael <dammydeji.dd@gmail.com>
2024-05-09 11:08:45 +0100

    personel_class_created

---
+++ src/com/damilola/schoolmodel/Staff.java
@@ -0,0 +1,35 @@
+package com.damilola.schoolmodel;
+
+public class Staff {
+    private int staffId;
+    private String position;
+    private double salary;
+
+    public Staff(){
+
+    }
+
+    public int getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
